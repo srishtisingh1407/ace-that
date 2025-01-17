c46ecabe49c71a792116673af6b7c4460f0beabb
Srishtisingh1407 <srishtisingh140704@gmail.com>
2025-01-17 08:39:38 +0530

    dashboard add

---
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddNewInterview() {
+  return (
+    <div>AddNewInterview</div>
+  )
+}
+
+export default AddNewInterview
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,13 +1,20 @@
 import React, { Children } from 'react'
 import Header from './_components/Header'
 import DashboardLayout from './layout'
+import AddNewInterview from './_components/AddNewInterview'
 
-function dashboard() {
+function Dashboard() {
   return (
+    <div className='p-10'>
+    <h2 className='font-bold text-2xl'>Dashboard</h2>
+    <h2 className='text-gray-500'>Create and Start yout AI Mockup Interview</h2>
+
+
     <div>
-    
+      <AddNewInterview/>
+    </div>
       </div>
   )
 }
 
-export default dashboard
+export default Dashboard
