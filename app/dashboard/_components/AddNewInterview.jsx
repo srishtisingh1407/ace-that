ec0adbb03f3bf7d3ea85db44a55df038c91306d1
Srishtisingh1407 <srishtisingh140704@gmail.com>
2025-01-19 15:10:00 +0530

    add a dialog for the new interview

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 function AddNewInterview() {
+  const [openDialog, setOpenDialog] = useState(false);
   return (
-    <div>AddNewInterview</div>
-  )
+    <div>
+      <div
+        className="p-10 border rounded-lg bg-emerald-100 hover:scale-105 hover:shadow-md cursor-pointer transition-all"
+        onClick={() => setOpenDialog(true)}
+      >
+        <h2 className="font-medium text-xl text-center">+ Add new </h2>
+      </div>
+      <Dialog open={openDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+             
+            </DialogDescription> <div>
+                <Button  onClick={()=>setOpenDialog(false)}>Cancel</Button>
+                <Button>Start Interview</Button>
+              </div>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 }
 
-export default AddNewInterview
+export default AddNewInterview;
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -7,10 +7,10 @@ function Dashboard() {
   return (
     <div className='p-10'>
     <h2 className='font-bold text-2xl'>Dashboard</h2>
-    <h2 className='text-gray-500'>Create and Start yout AI Mockup Interview</h2>
+    <h2 className='text-emerald-500'>Create and Start yout AI Mockup Interview</h2>
 
 
-    <div>
+    <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
       <AddNewInterview/>
     </div>
       </div>
