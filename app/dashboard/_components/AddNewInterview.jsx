e1f391e009306dd21b41e5430291ec98c72d7270
Srishtisingh1407 <srishtisingh140704@gmail.com>
2025-01-19 15:48:31 +0530

    add interview details

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 function AddNewInterview() {
   const [openDialog, setOpenDialog] = useState(false);
@@ -24,13 +25,13 @@ function AddNewInterview() {
       <Dialog open={openDialog}>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle className="font-semibold text-2xl">
+            <DialogTitle className="font-semibold text-2xl text-emerald-700">
               Tell us more about your job Interview
             </DialogTitle>
              
             <DialogDescription asChild>
               <div>
-                <h2 >
+                <h2 className="">
                   Add details about your job position, Job description and years
                   of experience
                 </h2>
@@ -38,13 +39,21 @@ function AddNewInterview() {
                   <label className="text-emerald-800" htmlFor="">Job Role/ Job Position :</label>
                   <Input placeholder="Ex. Front-end Developer"></Input>
                 </div>
+                <div className=" mt-7 my-3">
+                  <label className="text-emerald-800" htmlFor="">Job Description/ Tech-stack (In short) :</label>
+                  <Textarea placeholder="Ex. Javascript, React,etc."/>
+                </div>
+                <div className=" mt-7 my-3">
+                  <label className="text-emerald-800" htmlFor="">Years of Experience :</label>
+                  <Input type="number" placeholder="Ex. 1"></Input>
+                </div>
               </div>
             </DialogDescription>
             <div className="flex gap-5 justify-end">
               <Button variant="ghost" onClick={() => setOpenDialog(false)}>
                 Cancel
               </Button>
-              <Button>Start Interview</Button>
+              <Button className="bg-emerald-700 hover:bg-emerald-900">Start Interview</Button>
             </div>
           </DialogHeader>
         </DialogContent>
